a09b4e8fc7303ad0825999bc8d9b70a7e2593a1e
Sheansuke <suerojean@gmail.com>
2023-11-14 23:25:07 -0400

    done: chapter 12

---
+++ src/(data)/server-actions/deleteInvoice.ts
@@ -0,0 +1,7 @@
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
